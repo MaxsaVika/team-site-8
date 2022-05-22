a4b2ca6cd1e8527ce4df170e0296801b0c8e27c7
MaxsaVika <96749210+MaxsaVika@users.noreply.github.com>
2022-05-22 16:04:39 +0000

    Deploying to gh-pages from @ MaxsaVika/team-site-8@522bfb4dc5e9996bae95ddb895ace5393ba798c1 🚀

--- src.6d98acff.js
+++ src.6d98acff.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-test/src.6d98acff.js.map
+//# sourceMappingURL=/team-site-8/src.6d98acff.js.map
